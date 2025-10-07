5a556790166b58727030f5f89d6a7f4b9ef0dc0e
Jolfudr <laurenziusurschel@gmail.com>
2025-10-07 16:13:35 +0200

    07.10.2025 -> Developing Begin, Pushing Structre and Basics

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,6 +1,13 @@
 -- :h options | :h vim.opt
 local o = vim.opt
 
+-- shell settings in nvim
+
+o.shell = "pwsh"
+o.shellcmdflag = "-NoProfile -NoLogo -Command"
+o.shellquote = ""
+o.shellxquote = ""
+
 -- shiftwidth
 o.shiftwidth = 5
 
